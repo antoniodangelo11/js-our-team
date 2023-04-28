efcd11a96d86111486bf336091cbf2d277f6e175
antoninodangelo11 <reapercsp@gmail.com>
2023-04-28 14:35:09 +0200

    add html style and script.js. Add team Array

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede (se non avete idee prendete spunto dallo screenshot)
+*/
+
+const arrTeam = [
+    {
+        memberName: 'Wayne Barnett',
+        memberRole: 'Founder & CEO',
+        memberPic: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        memberName: 'Angela Caroll',
+        memberRole: 'Chief Editor',
+        memberPic: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        memberName: 'Walter Gordon',
+        memberRole: 'Office Manager',
+        memberPic: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        memberName: 'Angela Lopez',
+        memberRole: 'Social Media Manager',
+        memberPic: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        memberName: 'Scott Estrada',
+        memberRole: 'Developer',
+        memberPic: 'scott-estrada-developer.jpg',
+    },
+    {
+        memberName: 'Barbara Ramos',
+        memberRole: 'Graphic Designer',
+        memberPic: 'barbara-ramos-graphic-designer.jpg',
+    },
+
+];
+
+console.log(arrTeam);
